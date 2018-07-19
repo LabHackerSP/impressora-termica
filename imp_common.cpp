98050c998fe42c46f8d8bd4ae6728e9961371fae
Erin Pinheiro <Lana-chan@users.noreply.github.com>
2018-07-19 21:33:56 +0000

    funções serial para impressão

--- imp_common.cpp
+++ imp_common.cpp
@@ -5,5 +5,16 @@ SoftwareSerial impSerial(RX_PIN, TX_PIN);
 void commonSetup() {
   pinMode(BUTTON, INPUT_PULLUP);
   impSerial.begin(19200);
+  // printer init
+  impSerial.printf("\x1B\x40");
+}
+
+void serialPrint(char* string) {
+  impSerial.println(string);
+}
+
+void serialMode(byte printMode) {
+  impSerial.printf("\x1B\x21");
+  impSerial.print(printMode);
 }
 
--- imp_common.h
+++ imp_common.h
@@ -10,6 +10,17 @@ const byte RX_PIN = 14;
 const byte TX_PIN = 12;
 const byte BUTTON = 13;
 
+const byte MODE_FONT_B       = 1;
+const byte MODE_REVERSE      = 2;
+const byte MODE_UPSIDEDOWN   = 4;
+const byte MODE_EMPHASIZED   = 8;
+const byte MODE_DOUBLEHEIGHT = 16;
+const byte MODE_DOUBLEWIDTH  = 32;
+const byte MODE_DELETELINE   = 64;
+
 void commonSetup();
+
+void serialPrint(char*);
+void serialMode(byte);
 
 #endif // COMMON_H_
